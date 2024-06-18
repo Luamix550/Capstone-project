82a371f703a4007d614b927b8923a221253e2a13
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-06-17 19:31:18 -0500

    Add authentication routes, token handling, and initial profile setup for protected routes

---
+++ server/src/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const newUser = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    lastname: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+}, {
+    timestamps: true,
+});
+
+export default mongoose.model('User', newUser);
