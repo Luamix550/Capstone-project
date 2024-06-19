b1bfc436ec89b3c54f51f1b3341304865ee8ebd6
Luamix <luizalfonso611@gmail.com>
2024-06-19 10:18:51 -0500

    ADD component Login

---
+++ client/src/app/page.tsx
@@ -0,0 +1,11 @@
+import "./globals.css";
+import React from "react";
+import { Login } from "./components/login";
+
+export default function Home() {
+  return (
+    <>
+      <Login/>
+    </>
+  );
+}
