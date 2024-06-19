a4f52f47d4b9df9d29dc56a27836b6284e225f99
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-06-19 15:17:00 -0500

    Schema route is created which will validate if the data that the user is entering is correct, and validations for users are added

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import Router from 'express';
 import { registerUser, loginUser, profile, logout } from '../controllers/auth.controller.js';
 import { authRequired } from '../middlewares/validateToken.js';
+import { validateSchema } from '../middlewares/validateSchema.js';
+import { userRegisterSchema, userLoginSchema} from '../schemas/auth.schema.js';
 
 const router = Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('/register', validateSchema(userRegisterSchema), registerUser);
+router.post('/login', validateSchema(userLoginSchema), loginUser);
 router.post('/logout', logout);
 router.get('/profile', authRequired, profile);
 
