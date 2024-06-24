46fe87f29537c3254293e6619d80c2c58c3d27f5
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-06-24 13:05:44 -0500

    Updating login interface connection to the database

--- client/src/app/components/LoginForm.jsx
+++ client/src/app/components/LoginForm.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 
 const Login = () => {
+
+  const register = (formData) => {
+    const name = formData.get("name");
+    const lastname = formData.get("lastname");
+    const email = formData.get("email");
+    const password = formData.get("password");
+    
+    axios({
+      method: 'post',
+      url: 'http://localhost:3000/api/register',
+      data: {
+        name,
+        lastname,
+        email,
+        password,
+      }
+    })
+    
+  }
+
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="relative flex flex-col text-gray-700 bg-grey shadow-lg rounded-xl p-6 bg-clip-border">
         <h4 className="block font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900 text-center">
           Sign Up
         </h4>
-        <form className="max-w-screen-lg mt-8 mb-2 w-80 sm:w-96">
+        <form className="max-w-screen-lg mt-8 mb-2 w-80 sm:w-96" action={register}>
           <div className="flex flex-col gap-6 mb-1">
             <h6 className="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
               Your Name
@@ -15,6 +36,18 @@ const Login = () => {
             <div className="relative h-11 w-full min-w-[200px]">
               <input
                 placeholder="John Doe"
+                name='name'
+                className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+              />
+            </div>
+            <h6 className="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
+              Lastname
+            </h6>
+            <div className="relative h-11 w-full min-w-[200px]">
+              <input
+                type="text"
+                name='lastname'
+                placeholder="Last name"
                 className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
               />
             </div>
@@ -23,7 +56,9 @@ const Login = () => {
             </h6>
             <div className="relative h-11 w-full min-w-[200px]">
               <input
+              type='email'
                 placeholder="name@mail.com"
+                name='email'
                 className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
               />
             </div>
@@ -32,6 +67,7 @@ const Login = () => {
             </h6>
             <div className="relative h-11 w-full min-w-[200px]">
               <input
+                name='password'
                 type="password"
                 placeholder="********"
                 className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
@@ -53,7 +89,7 @@ const Login = () => {
           </div>
           <button
             className="mt-6 block w-full select-none rounded-lg bg-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            type="button"
+            type="submit"
           >
             Sign Up
           </button>
