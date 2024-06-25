6c31a952b16bcb5f2dbc9e647e90d3bc69c9213e
Luamix550 <luizalfonso611@gmail.com>
2024-06-25 17:04:16 -0500

    Resolve conflics in Login

--- client/src/app/components/LoginForm.jsx
+++ client/src/app/components/LoginForm.jsx
@@ -2,7 +2,6 @@ import axios from 'axios';
 import React from 'react';
 
 const Login = () => {
-<<<<<<< HEAD
 
   const register = async (formData) => {
     const name = formData.get('name');
@@ -36,34 +35,6 @@ const Login = () => {
                 name='name'
                 placeholder="Enter name"
                 className="peer h-11 w-full rounded-md border border-blue-gray-200 bg-transparent px-3 py-2 font-sans text-sm text-blue-gray-700 outline-none transition-all focus:border-2 focus:border-gray-900"
-=======
-  return (
-    <div className="flex items-center justify-center min-h-screen min-h-2.5">
-      <div className="relative flex flex-col text-gray-700 bg-grey shadow-lg rounded-xl p-6 bg-clip-border">
-        <h4 className="block font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900 text-center">
-          Sign Up
-        </h4>
-        <form className="max-w-screen-lg mt-8 mb-2 w-80 sm:w-96">
-          <div className="flex flex-col gap-6 mb-1">
-            <h6 className="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
-              Name
-            </h6>
-            <div className="relative h-11 w-full min-w-[200px]">
-              <input
-                name='name'
-                placeholder="John Doe"
-                className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-              />
-            </div>
-            <h6 className="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
-              Last name
-            </h6>
-            <div className="relative h-11 w-full min-w-[200px]">
-              <input
-                name='lastname'
-                placeholder="John Doe"
-                className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
->>>>>>> 1044e60a275ed0c16233b1b79c7a7a28a1083343
               />
             </label>
             <label className="block font-sans text-base font-semibold leading-relaxed text-blue-gray-900">
@@ -73,20 +44,10 @@ const Login = () => {
                 placeholder="Enter last name"
                 className="peer h-11 w-full rounded-md border border-blue-gray-200 bg-transparent px-3 py-2 font-sans text-sm text-blue-gray-700 outline-none transition-all focus:border-2 focus:border-gray-900"
               />
-<<<<<<< HEAD
             </label>
             <label className="block font-sans text-base font-semibold leading-relaxed text-blue-gray-900">
               Email address
               <input
-=======
-            </div>
-            <h6 className="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
-              Email address
-            </h6>
-            <div className="relative h-11 w-full min-w-[200px]">
-              <input
-                placeholder="name@mail.com"
->>>>>>> 1044e60a275ed0c16233b1b79c7a7a28a1083343
                 name='email'
                 type='email'
                 placeholder="Enter mail address"
