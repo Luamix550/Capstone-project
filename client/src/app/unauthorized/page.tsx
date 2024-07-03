68e382aabdac81c84c11cbe5beffcc38af40ebda
foultrip <davidvasquezmahecha@gmail.com>
2024-07-03 12:19:16 -0500

    add useEffect hook in Unauthorized component

--- client/src/app/unauthorized/page.tsx
+++ client/src/app/unauthorized/page.tsx
@@ -1,19 +1,22 @@
-"use client"
-import React from 'react';
-import { useRouter } from 'next/navigation';
+"use client";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const Unauthorized = () => {
   const router = useRouter();
 
-  const returnHomePage = () => {
-    setTimeout(() => {
-      router.push('/started');
-    }, 5000);
-  }
+  useEffect(() => {
+    const returnHomePage = () => {
+      setTimeout(() => {
+        router.push("/started");
+      }, 5000);
+    };
+
+    returnHomePage();
+  }, [router]);
+
   return (
-    <div className="bg-unauthorized relative h-screen bg-cover bg-center flex flex-col justify-centerbg-cover">
-      {returnHomePage()}
-    </div>
+    <div className="bg-unauthorized relative h-screen bg-cover bg-center flex flex-col justify-centerbg-cover" />
   );
 };
 
